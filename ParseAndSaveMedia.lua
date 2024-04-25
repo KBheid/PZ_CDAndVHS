d22b995ef884329fbf2d2081336402bac7950d3b
KBHeid <kbheid@live.com>
2024-04-25 12:26:18 -0500

    %ifis% tag, code decoding

--- ParseAndSaveMedia.lua
+++ ParseAndSaveMedia.lua
@@ -1,16 +1,16 @@
 local json = require('dkjson')
 
-pzPath = arg[1]
+local pzPath = arg[1]
 
 function loadMediaLuaFile()
-	mediaLuaPath = pzPath .. "\\media\\lua\\shared\\RecordedMedia\\recorded_media.lua"
+	local mediaLuaPath = pzPath .. [[\media\lua\shared\RecordedMedia\recorded_media.lua]]
 	dofile(mediaLuaPath)
 end
 
 function saveMediaTable()
 	local js = json.encode(RecMedia, {indent = true})
 	
-	local output = io.open(arg[2] .. "\\intermediate.out", 'w')
+	local output = io.open(arg[2] .. [[\intermediate.out]], 'w')
 	output:write(js)
 	output:close()
 end
